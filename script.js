47a7dfd9b53d18b9bfd0d999042fb098a5c93a70
Aman Tripathi <imamantripathi@gmail.com>
2022-04-30 13:30:33 +0530

    #15

--- script.js
+++ script.js
@@ -7,4 +7,33 @@ hamburger_menu.addEventListener("click", () => {
 
 // Gallery
 
+// core Team
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
 
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+//scroll up button 
+  var btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+  
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+  
