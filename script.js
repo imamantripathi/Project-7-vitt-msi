245bce2ebc90dc79a9d1163015ef66f759a5acb6
Aman Tripathi <imamantripathi@gmail.com>
2022-05-21 13:46:27 +0530

    contact-form-handler using php

--- script.js
+++ script.js
@@ -26,4 +26,10 @@ $(".hover").mouseleave(
    window.scrollTo(0,0);
   
   } 
-    
+  // form validation 
+  
+var frmvalidator  = new Validator("contact_form");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
