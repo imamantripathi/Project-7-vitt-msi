14e42ea5ccd07530e242f95e08c4c47b6ccfe28b
Aman Tripathi <imamantripathi@gmail.com>
2022-06-12 10:53:52 +0530

    Responsive #3

--- script.js
+++ script.js
@@ -26,10 +26,4 @@ $(".hover").mouseleave(
    window.scrollTo(0,0);
   
   } 
-  // form validation 
   
-var frmvalidator  = new Validator("contact_form");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
